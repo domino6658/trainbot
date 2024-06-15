8e41d639c5bcbbe36d3be5703798bae775420e9c
domino6658 <123335575+domno6658@users.noreply.github.com>
2024-06-15 13:07:45 +1000

    fix ptv api request

--- utils/keyCalc.py
+++ utils/keyCalc.py
@@ -1,20 +1,21 @@
 from hashlib import sha1
 import hmac
-from dotenv import dotenv_values
 import os
 import yaml
 import utils.getConfig as getConfig
-
 
 def getUrl(request):
     config = getConfig.getconfig()
 
     devId = int(config['ptv_api']['dev_id'])
-    key = str(config['ptv_api']['dev_id'])
-    
-    request = request + ('&' if ('?' in request) else '?')
-    raw = request+'devid={0}'.format(devId)
-    hashed = hmac.new(key, raw, sha1)
-    signature = hashed.hexdigest()
+    key = config['ptv_api']['key'].encode('UTF-8')
 
-    return 'http://timetableapi.ptv.vic.gov.au' + raw + '&signature={1}'.format(devId, signature)
+    request = request + ('&' if ('?' in request) else '?')
+    raw = request + 'devid={0}'.format(devId)
+    
+    # Encode the raw string
+    raw_encoded = raw.encode('utf-8')
+    
+    hashed = hmac.new(key, raw_encoded, sha1)
+    signature = hashed.hexdigest()
+    return 'https://timetableapi.ptv.vic.gov.au' + raw + '&signature={1}'.format(devId, signature)

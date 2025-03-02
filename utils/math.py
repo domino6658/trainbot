9856bb85f498e1de832995e6921baa54c3b3fb33
domino6658 <dlpanozzo1234@gmail.com>
2025-03-02 23:49:54 +1100

    Make numbers integers

--- utils/math.py
+++ utils/math.py
@@ -24,10 +24,10 @@ def haversine(lat1, lon1, lat2, lon2):
     R = 6371.0
 
     # Convert degrees to radians
-    lat_rad1 = math.radians(lat1)
-    lon_rad1 = math.radians(lon1)
-    lat_rad2 = math.radians(lat2)
-    lon_rad2 = math.radians(lon2)
+    lat_rad1 = int(math.radians(lat1))
+    lon_rad1 = int(math.radians(lon1))
+    lat_rad2 = int(math.radians(lat2))
+    lon_rad2 = int(math.radians(lon2))
 
     # Differences in coordinates
     delta_lat = lat_rad2 - lat_rad1
